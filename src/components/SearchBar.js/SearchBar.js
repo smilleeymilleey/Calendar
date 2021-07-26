543ef071200eb227c314dcd79d4e1cc8883262ba
Caitlyn <caitlynmiley.cm@gmail.com>
2021-07-26 11:44:50 -0500

    used state to grab value of imput bar

--- src/components/SearchBar.js/SearchBar.js
+++ src/components/SearchBar.js/SearchBar.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import getData from "../../API.js"
 import "./SearchBar.css"
 
 function SearchBar() {
+    const [ search, setSearch ] = useState()
+
+    function getSearchValue(e) {
+        let searchValue=e.target.value
+        console.log(searchValue)
+    }
     return (
         <div>
             <div className="search">
-                <input placeholder="search"/>
+                <input placeholder="search" value = {search} onChange={getSearchValue}/>
                 <button onClick={()=> getData()}>button</button>
             </div> 
         </div>
-    )
+)
 }
 
 export default SearchBar
