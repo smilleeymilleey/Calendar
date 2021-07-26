b47e329cf215091a55511845cabc946a8dde9a6f
Caitlyn <caitlynmiley.cm@gmail.com>
2021-07-26 18:01:42 -0500

    styled buttons and added a line on card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,22 +7,20 @@ import "./card.css"
 function Card({ word, definition }) {
     return (
         <div>
-            <div class="card">
-            <div class="card-image">
+            <div className="card">
+            <div className="card-image">
             </div>
-            <div class="card-content">
-                <div class="media">
-                <div class="media-left">
+            <div className="card-content">
+                <div className="media">
+                <div className="media-left">
                 </div>
-                <div class="media-content">
-                    <p class="title is-4">{word.entry}</p>
-                 
+                <div className="media-content">
+                    <p className="title is-4">{word.entry}</p>
+                    <hr></hr>
                 </div>
                 </div>
-                <div class="content">
-                    <p class="subtitle is-6">{definition.lexemes[0].senses[0].definition}</p>
-                <br></br>
-                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                <div className="content">
+                    <p className="subtitle is-6">{definition.lexemes[0].senses[0].definition}</p>
                 </div>
             </div>
             </div>
