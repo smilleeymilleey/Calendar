31d2a6d0f53f8c427ca39ec13c4593fd5aa8c230
Caitlyn <caitlynmiley.cm@gmail.com>
2021-07-27 11:17:03 -0500

    styled audio button with material icon

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,19 +6,6 @@ import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects';
 
 
 function Card({ word, definition, partSpeech, sound }) {
-    function isAudioAvailable( props ) {
-        const isAudio = props.isAudio
-        if (isAudio) {
-            return 
-            <EmojiObjectsIcon href={sound.pronunciations[0].audio.url}/>
-        }
-        return (
-            <h1>No Audio Available</h1>
-        )
-    }
-
-
-
     return (
         <div>
             <div className="card">
@@ -29,17 +16,12 @@ function Card({ word, definition, partSpeech, sound }) {
                 <div className="media-left">
                 </div>
                 <div className="media-content">
-                    <p className="title is-4">{word.entry}</p> 
+                    <p className="title is-4">{word.entry}
                         {
-                    
-                    
-                         sound.pronunciations[0].audio?.url &&
-                               
-                          <EmojiObjectsIcon href={sound.pronunciations[0].audio.url}/>
-                            
-                         
-                            
+                         sound.pronunciations[0].audio?.url &&   
+                          <EmojiObjectsIcon id="sound" href={sound.pronunciations[0].audio.url}/> 
                         } 
+                    </p>
                     <hr></hr>
                     <p id="speech" className="subtitle is-6">{partSpeech.lexemes[0].partOfSpeech}</p>
                 </div>
