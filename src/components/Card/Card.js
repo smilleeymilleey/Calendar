d238efc41a5c6e5a1adb8f362ef9097f2b639891
Caitlyn <caitlynmiley.cm@gmail.com>
2021-07-27 18:15:31 -0500

    added a modal onclick of lightbulb button for speach box to go

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react'
+import Modal from'react-modal';
 import "./card.css"
 import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects';
 
 
 function Card({ word, definition, partSpeech, sound, pro }) {
-    const [ modal, setModal] = useState()
+    const [ modalOpen, setModalOpen] = useState(false)
 
-        
-
+    const setModalOpenToTrue =()=>{
+        setModalOpen(true)
+    }
+    
+    const setModalOpenToFalse =()=>{
+        setModalOpen(false)
+    }
     return (
         <div>
             <div className="card">
@@ -18,22 +24,21 @@ function Card({ word, definition, partSpeech, sound, pro }) {
                 <div className="media-left">
                 </div>
                 <div className="media-content">
-                    <p className="title is-4">{word.entry}
+                    <p className="title is-4">{word.entry} </p>
                    
                      {
                         sound.pronunciations[0].audio?.url && 
-                        <button id="myBtn" href={sound.pronunciations[0].audio.url}>
+                        <button id="myBtn"  onClick={setModalOpenToTrue} href={sound.pronunciations[0].audio.url}>
                             <EmojiObjectsIcon id="sound"/> 
                         </button>  
                       }  
-                      <div id="myModal" onClick={() => modal.style.display = "block"} className="modal">
-                                Modal content 
-                        <div className="modal-content">
-                            <span className="close">&times;</span>
-                            <p>Some text in the Modal..</p>
-                        </div>
-                        </div>   
-                    </p>
+                      <div id="myModal"  className="modal">
+                                <h1>Content</h1>
+                     </div>  
+                     <Modal isOpen={modalOpen}>
+                         <button onClick={setModalOpenToFalse}>X</button>
+                     </Modal>
+                   
                     <p>{pro.pronunciations[0].transcriptions[0].transcription}</p>
                     <hr></hr>
                     <p id="speech" className="subtitle is-6">{partSpeech.lexemes[0].partOfSpeech}</p>
