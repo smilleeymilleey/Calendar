618e913157ee5701c5fde61c803d4620815a3e07
Caitlyn <caitlynmiley.cm@gmail.com>
2021-07-27 17:20:37 -0500

    added start to modal'

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./card.css"
 import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects';
 
 
 function Card({ word, definition, partSpeech, sound, pro }) {
+    const [ modal, setModal] = useState()
+
+        
+
     return (
         <div>
             <div className="card">
@@ -15,12 +19,20 @@ function Card({ word, definition, partSpeech, sound, pro }) {
                 </div>
                 <div className="media-content">
                     <p className="title is-4">{word.entry}
+                   
                      {
                         sound.pronunciations[0].audio?.url && 
-                        <a href={sound.pronunciations[0].audio.url}>
+                        <button id="myBtn" href={sound.pronunciations[0].audio.url}>
                             <EmojiObjectsIcon id="sound"/> 
-                        </a>  
-                      }     
+                        </button>  
+                      }  
+                      <div id="myModal" onClick={() => modal.style.display = "block"} className="modal">
+                                Modal content 
+                        <div className="modal-content">
+                            <span className="close">&times;</span>
+                            <p>Some text in the Modal..</p>
+                        </div>
+                        </div>   
                     </p>
                     <p>{pro.pronunciations[0].transcriptions[0].transcription}</p>
                     <hr></hr>
@@ -31,7 +43,7 @@ function Card({ word, definition, partSpeech, sound, pro }) {
                 </div>
                 </div>
             </div>
-            </div>
+            </div>  
         </div>
     )
 }
